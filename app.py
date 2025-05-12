0e2fe337486118bee2edc54bedf857cb5b255ac2
Sagarika-project <sagarikamaity92@gmail.com>
2025-05-12 14:20:56 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import pickle
+import numpy as np
+
+# Load model & scaler
+with open("heart_disease_model.pkl", "rb") as model_file:
+    model = pickle.load(model_file)
+
+with open("scaler.pkl", "rb") as scaler_file:
+    scaler = pickle.load(scaler_file)
+
+# Define feature names
+selected_features = ["age", "sex", "cp", "trestbps", "chol", "thalach", "exang", "oldpeak", "ca", "thal"]
+
+app = FastAPI()
+class HeartData(BaseModel):
+    data: dict
+@app.api_route("/", methods=["GET", "HEAD"])
+def home():
+    return {"message": "FastAPI is successfully running on Render!"}
+@app.get("/hii")
+async def home_root():
+    return {"message": "Welcome to the Heart Disease API"}
+@app.post("/predict")
+def predict(data: HeartData):
+    input_data = [data.data[feature] for feature in selected_features]  # Extract values in correct order
+    X_scaled = scaler.transform([input_data])
+    prediction = model.predict(X_scaled)
+    return {"prediction": int(prediction[0])}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+
+
+
