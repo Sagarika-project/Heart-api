47526c886f0696f17121e1fbb073b48296e956cf
Sagarika-project <sagarikamaity92@gmail.com>
2025-05-18 12:28:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,8 @@ def predict(data: HeartData):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port=int(os.getenv("POST",8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
 
 
 
